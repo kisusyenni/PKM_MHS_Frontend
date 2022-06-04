dbff2a29b263ff500fd644189a244b61c990841e
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-04 21:42:01 +0700

    Add and Edit Inventory UI

---
+++ src/views/inventory/AddInventory.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { CButton, CCol, CContainer, CRow } from "@coreui/react";
+import InventoryForm from "./InventoryForm";
+import CIcon from "@coreui/icons-react";
+import { cilArrowCircleLeft } from "@coreui/icons";
+import { useNavigate } from "react-router-dom";
+
+const AddInventory = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <CContainer>
+                <CRow>
+                    <CCol md={9} lg={7} xl={6}>
+                        <CButton color="link" className="mb-2" onClick={() => navigate(-1)}>
+                            <CIcon className="me-2" icon={cilArrowCircleLeft} />
+                            Kembali
+                        </CButton>
+                        <InventoryForm title="Tambah Inventaris" isEdit={false} />
+                    </CCol>
+                </CRow>
+            </CContainer>
+        </>
+    );
+};
+
+export default AddInventory;
---
+++ src/views/inventory/EditInventory.js
@@ -0,0 +1,28 @@
+import { cilArrowCircleLeft } from "@coreui/icons";
+import CIcon from "@coreui/icons-react";
+import { CButton, CCol, CContainer, CRow } from "@coreui/react";
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import InventoryForm from "./InventoryForm";
+
+const EditInventory = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    return (
+        <>
+            <CContainer>
+                <CRow>
+                    <CCol md={9} lg={7} xl={6}>
+                        <CButton color="link" className="mb-2" onClick={() => navigate(-1)}>
+                            <CIcon className="me-2" icon={cilArrowCircleLeft} />
+                            Kembali
+                        </CButton>
+                        <InventoryForm title={"Ubah Inventaris"} isEdit={true} />
+                    </CCol>
+                </CRow>
+            </CContainer>
+        </>
+    );
+};
+
+export default EditInventory;
