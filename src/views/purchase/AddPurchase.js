1ce8dde0a71d9ce4c5ada0e2be3539a83be3faaf
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-09 11:28:57 +0700

    Create AddPurchase and EditPurchase component

---
+++ src/views/purchase/AddPurchase.js
@@ -0,0 +1,15 @@
+import { CContainer } from "@coreui/react";
+import React from "react";
+import PurchaseForm from "./PurchaseForm";
+
+const AddPurchase = () => {
+    return (
+        <>
+            <CContainer>
+                <PurchaseForm />
+            </CContainer>
+        </>
+    );
+};
+
+export default AddPurchase;
---
+++ src/views/purchase/EditPurchase.js
@@ -0,0 +1,12 @@
+import { CContainer } from "@coreui/react";
+import React from "react";
+
+const EditPurchase = () => {
+    return (
+        <>
+            <CContainer></CContainer>
+        </>
+    );
+};
+
+export default EditPurchase;
