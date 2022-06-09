27fa3b946a60c9b584335a659699b2005c9256dc
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-09 11:29:24 +0700

    Create PurchaseDetail and PurchasePaymentForm component

---
+++ src/views/purchase/PurchaseDetail.js
@@ -0,0 +1,12 @@
+import { CContainer } from "@coreui/react";
+import React from "react";
+
+const PurchaseDetail = () => {
+    return (
+        <>
+            <CContainer></CContainer>
+        </>
+    );
+};
+
+export default PurchaseDetail;
---
+++ src/views/purchase/PurchasePaymentForm.js
@@ -0,0 +1,15 @@
+import { CCard, CCardBody, CCardHeader } from "@coreui/react";
+import React from "react";
+
+const PurchasePaymentForm = () => {
+    return (
+        <>
+            <CCard>
+                <CCardHeader></CCardHeader>
+                <CCardBody></CCardBody>
+            </CCard>
+        </>
+    );
+};
+
+export default PurchasePaymentForm;
