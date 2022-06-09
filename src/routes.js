c433257b79dd61ccd05a6cf0d5eea388405150f5
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-09 11:27:20 +0700

    Rename purchase route

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const AddInventory = React.lazy(() => import("./views/inventory/AddInventory"));
 const EditInventory = React.lazy(() => import("./views/inventory/EditInventory"));
 const Supplier = React.lazy(() => import("./views/supplier/Supplier"));
 const Purchase = React.lazy(() => import("./views/purchase/Purchase"));
+const AddPurchase = React.lazy(() => import("./views/purchase/AddPurchase"));
 
 const routes = [
     { path: "/", exact: true, name: "Home" },
@@ -17,7 +18,8 @@ const routes = [
     { path: "/inventaris/tambah", name: "Tambah Inventaris", element: AddInventory },
     { path: "/inventaris/ubah/:id", name: "Ubah Inventaris", element: EditInventory },
     { path: "/supplier", name: "Supplier", element: Supplier },
-    { path: "/penjualan", name: "Penjualan", element: Purchase },
+    { path: "/pembelian", name: "Pembelian", element: Purchase },
+    { path: "/pembelian/tambah", name: "Tambah Pembelian", element: AddPurchase },
 ];
 
 export default routes;
