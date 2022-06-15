69e3c1127d7cb0ffbc71867da9ee32da65ef2403
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-15 21:23:02 +0700

    Modify routes

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,8 @@ const Inventory = React.lazy(() => import("./views/inventory/Inventory"));
 const AddInventory = React.lazy(() => import("./views/inventory/AddInventory"));
 const EditInventory = React.lazy(() => import("./views/inventory/EditInventory"));
 const Supplier = React.lazy(() => import("./views/supplier/Supplier"));
-const Purchase = React.lazy(() => import("./views/purchase/Purchase"));
-const AddPurchase = React.lazy(() => import("./views/purchase/AddPurchase"));
+const Purchase = React.lazy(() => import("./views/purchase/purchase-list/Purchase"));
+const AddPurchase = React.lazy(() => import("./views/purchase/purchase-form/AddPurchase"));
 
 const routes = [
     { path: "/", exact: true, name: "Home" },
