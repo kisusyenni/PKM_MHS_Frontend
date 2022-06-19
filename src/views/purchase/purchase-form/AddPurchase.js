527a3b03ae483d161cc44184e783cf74390b11de
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-19 19:21:07 +0700

    Add back button to add purchase form

--- src/views/purchase/purchase-form/AddPurchase.js
+++ src/views/purchase/purchase-form/AddPurchase.js
@@ -1,11 +1,21 @@
-import { CContainer } from "@coreui/react";
 import React from "react";
+import { CButton, CContainer } from "@coreui/react";
+import { useNavigate } from "react-router-dom";
 import PurchaseForm from "./PurchaseForm";
 
 const AddPurchase = () => {
+    const navigate = useNavigate();
     return (
         <>
             <CContainer>
+                <CButton
+                    className="mb-3"
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                >
+                    Back
+                </CButton>
                 <PurchaseForm />
             </CContainer>
         </>
