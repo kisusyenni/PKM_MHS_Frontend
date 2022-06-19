0f30a81adb88c7b987defed9da12a7213a976c97
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-19 19:14:30 +0700

    Create add sales form component

---
+++ src/views/sales/sales-form/AddSales.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { CButton, CContainer } from "@coreui/react";
+import SalesForm from "./SalesForm";
+import { useNavigate } from "react-router-dom";
+
+const AddSales = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <CContainer>
+                <CButton
+                    className="mb-3"
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                >
+                    Back
+                </CButton>
+                <SalesForm />
+            </CContainer>
+        </>
+    );
+};
+
+export default AddSales;
