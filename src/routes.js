f132dd2b631704e5ad1af0409cc220488fd7d486
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-19 19:20:56 +0700

    Add route for sales

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,21 @@ import React from "react";
 const Dashboard = React.lazy(() => import("./views/dashboard/Dashboard"));
 const EditStore = React.lazy(() => import("./views/store/EditStore"));
 const Report = React.lazy(() => import("./views/reports/Report"));
+
+const Supplier = React.lazy(() => import("./views/supplier/Supplier"));
+
 const Inventory = React.lazy(() => import("./views/inventory/Inventory"));
 const AddInventory = React.lazy(() => import("./views/inventory/AddInventory"));
 const EditInventory = React.lazy(() => import("./views/inventory/EditInventory"));
-const Supplier = React.lazy(() => import("./views/supplier/Supplier"));
+
 const Purchase = React.lazy(() => import("./views/purchase/purchase-list/Purchase"));
 const PurchaseDetail = React.lazy(() => import("./views/purchase/purchase-detail/PurchaseDetail"));
 const AddPurchase = React.lazy(() => import("./views/purchase/purchase-form/AddPurchase"));
+
+const Sales = React.lazy(() => import("./views/sales/sales-list/Sales"));
+const SalesDetail = React.lazy(() => import("./views/sales/sales-detail/SalesDetail"));
+const AddSales = React.lazy(() => import("./views/sales/sales-form/AddSales"));
+
 const Expense = React.lazy(() => import("./views/expense/Expense"));
 const AddExpense = React.lazy(() => import("./views/expense/AddExpense"));
 
@@ -17,13 +25,20 @@ const routes = [
     { path: "/dashboard", name: "Dashboard", element: Dashboard },
     { path: "/edit-toko", name: "Edit Toko", element: EditStore },
     { path: "/laporan", name: "Laporan", element: Report },
+    { path: "/supplier", name: "Supplier", element: Supplier },
+
     { path: "/inventaris", name: "Inventaris", element: Inventory },
     { path: "/inventaris/tambah", name: "Tambah Inventaris", element: AddInventory },
     { path: "/inventaris/ubah/:id", name: "Ubah Inventaris", element: EditInventory },
-    { path: "/supplier", name: "Supplier", element: Supplier },
+
     { path: "/pembelian", name: "Pembelian", element: Purchase },
     { path: "/pembelian/:id", name: "Detail Pembelian", element: PurchaseDetail },
     { path: "/pembelian/tambah", name: "Tambah Pembelian", element: AddPurchase },
+
+    { path: "/penjualan", name: "Penjualan", element: Sales },
+    { path: "/penjualan/:id", name: "Detail Penjualan", element: SalesDetail },
+    { path: "/penjualan/tambah", name: "Tambah Penjualan", element: AddSales },
+
     { path: "/pengeluaran", name: "Pengeluaran", element: Expense },
     { path: "/pengeluaran/tambah", name: "Tambah Pengeluaran", element: AddExpense },
 ];
