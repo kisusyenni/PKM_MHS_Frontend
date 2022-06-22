07da1130ca1c632390038b637445f7e0d5c31e1b
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-22 16:46:45 +0700

    Create stock opname form UI

---
+++ src/views/stock-opname/AddStockOpname.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddStockOpname = () => {
+    return <div>AddStockOpname</div>;
+};
+
+export default AddStockOpname;
---
+++ src/views/stock-opname/StockOpnameForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StockOpnameForm = () => {
+    return <div>StockOpnameForm</div>;
+};
+
+export default StockOpnameForm;
