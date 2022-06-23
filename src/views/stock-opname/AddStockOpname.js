327b86edf12413057847eef16b1ab4b92c1254c2
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-23 23:11:58 +0700

    Create add stock opname UI

--- src/views/stock-opname/AddStockOpname.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddStockOpname = () => {
-    return <div>AddStockOpname</div>;
-};
-
-export default AddStockOpname;
---
+++ src/views/stock-opname/stock-opname-form/AddStockOpname.js
@@ -0,0 +1,23 @@
+import { CButton } from "@coreui/react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import StockOpnameForm from "./StockOpnameForm";
+
+const AddStockOpname = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <CButton
+                className="mb-3"
+                onClick={() => {
+                    navigate(-1);
+                }}
+            >
+                Back
+            </CButton>
+            <StockOpnameForm />
+        </>
+    );
+};
+
+export default AddStockOpname;
