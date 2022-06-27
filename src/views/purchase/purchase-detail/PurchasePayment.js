95065801721961a7b6c8c24639b8f0c47d3725cb
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-06-27 16:03:03 +0700

    Modify UI

--- src/views/purchase/purchase-detail/PurchasePayment.js
+++ src/views/purchase/purchase-detail/PurchasePayment.js
@@ -7,14 +7,16 @@ import PaymentList from "src/components/payment/PaymentList";
 const PurchasePayment = ({ id, total, status }) => {
     return (
         <>
-            <CCard className="my-3">
-                <CCardHeader>
-                    <h2 className="h6 fw-bold my-2">Terima Pembayaran</h2>
-                </CCardHeader>
-                <CCardBody>
-                    {status !== 3 && <PaymentForm type={"purchase"} id={id} total={total} />}
-                </CCardBody>
-            </CCard>
+            {status !== 3 && (
+                <CCard className="my-3">
+                    <CCardHeader>
+                        <h2 className="h6 fw-bold my-2">Terima Pembayaran</h2>
+                    </CCardHeader>
+                    <CCardBody>
+                        <PaymentForm type={"purchase"} id={id} total={total} />
+                    </CCardBody>
+                </CCard>
+            )}
             <div className="my-3">
                 {status !== 1 && (
                     <PaymentList type={"purchase"} id={id} total={total} status={status} />
