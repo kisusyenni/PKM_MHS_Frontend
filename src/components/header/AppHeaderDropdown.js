712906f101b11222b54f3fb460d20f809065b2fa
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-07-02 00:48:10 +0700

    Modify header UI

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,7 +1,5 @@
 import React from "react";
 import {
-    CAvatar,
-    CBadge,
     CDropdown,
     CDropdownDivider,
     CDropdownHeader,
@@ -9,21 +7,9 @@ import {
     CDropdownMenu,
     CDropdownToggle,
 } from "@coreui/react";
-import {
-    cilBell,
-    cilCreditCard,
-    cilCommentSquare,
-    cilEnvelopeOpen,
-    cilFile,
-    cilLockLocked,
-    cilSettings,
-    cilTask,
-    cilUser,
-    cilAccountLogout,
-} from "@coreui/icons";
+import { cilSettings, cilAccountLogout, cilHouse } from "@coreui/icons";
 import CIcon from "@coreui/icons-react";
 
-import avatar8 from "./../../assets/images/avatars/8.jpg";
 import useAuth from "src/hooks/useAuth";
 
 const AppHeaderDropdown = () => {
@@ -31,60 +17,13 @@ const AppHeaderDropdown = () => {
     return (
         <CDropdown variant="nav-item">
             <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
-                <CAvatar src={avatar8} size="md" />
+                <CIcon icon={cilSettings} />
             </CDropdownToggle>
             <CDropdownMenu className="pt-0" placement="bottom-end">
-                <CDropdownHeader className="bg-light fw-semibold py-2">Account</CDropdownHeader>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilBell} className="me-2" />
-                    Updates
-                    <CBadge color="info" className="ms-2">
-                        42
-                    </CBadge>
-                </CDropdownItem>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilEnvelopeOpen} className="me-2" />
-                    Messages
-                    <CBadge color="success" className="ms-2">
-                        42
-                    </CBadge>
-                </CDropdownItem>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilTask} className="me-2" />
-                    Tasks
-                    <CBadge color="danger" className="ms-2">
-                        42
-                    </CBadge>
-                </CDropdownItem>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilCommentSquare} className="me-2" />
-                    Comments
-                    <CBadge color="warning" className="ms-2">
-                        42
-                    </CBadge>
-                </CDropdownItem>
-                <CDropdownHeader className="bg-light fw-semibold py-2">Settings</CDropdownHeader>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilUser} className="me-2" />
-                    Profile
-                </CDropdownItem>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilSettings} className="me-2" />
-                    Settings
-                </CDropdownItem>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilCreditCard} className="me-2" />
-                    Payments
-                    <CBadge color="secondary" className="ms-2">
-                        42
-                    </CBadge>
-                </CDropdownItem>
-                <CDropdownItem href="#">
-                    <CIcon icon={cilFile} className="me-2" />
-                    Projects
-                    <CBadge color="primary" className="ms-2">
-                        42
-                    </CBadge>
+                <CDropdownHeader className="bg-light fw-semibold py-2">Pengaturan</CDropdownHeader>
+                <CDropdownItem href="/edit-toko">
+                    <CIcon icon={cilHouse} className="me-2" />
+                    Ubah Toko
                 </CDropdownItem>
                 <CDropdownDivider />
                 <CDropdownItem onClick={logout}>
