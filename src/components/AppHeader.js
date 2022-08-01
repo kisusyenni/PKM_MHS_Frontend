74b08aeca1c695ea8a52fd5eca87dd38faad6c8a
Kisusyenni Venessa <kisusyenni01@gmail.com>
2022-08-01 10:34:23 +0700

    Fix header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -10,13 +10,14 @@ import {
     CHeaderToggler,
     CNavLink,
     CNavItem,
+    CImage,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
 import { cilMenu } from "@coreui/icons";
 
 import { AppBreadcrumb } from "./index";
 import { AppHeaderDropdown } from "./header/index";
-import { logo } from "src/assets/brand/logo";
+import logo from "src/assets/brand/logo.png";
 
 const AppHeader = () => {
     const dispatch = useDispatch();
@@ -31,8 +32,8 @@ const AppHeader = () => {
                 >
                     <CIcon icon={cilMenu} size="lg" />
                 </CHeaderToggler>
-                <CHeaderBrand className="mx-auto d-md-none" to="/">
-                    <CIcon icon={logo} height={48} alt="Logo" />
+                <CHeaderBrand className="ms-auto d-md-none" to="/">
+                    <CImage src={logo} height={50} />
                 </CHeaderBrand>
                 <CHeaderNav className="d-none d-md-flex me-auto">
                     <CNavItem>
